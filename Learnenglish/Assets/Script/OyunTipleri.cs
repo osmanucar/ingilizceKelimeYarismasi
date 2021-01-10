6ad27d7cd9c2ab8feab1d7d7556e33360766f461
osmanucar <63417248+osmanucar@users.noreply.github.com>
2021-01-10 15:23:12 +0300

    güncelleme

---
+++ Learnenglish/Assets/Script/OyunTipleri.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OyunTipleri : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ing_B()
+    {
+        SahneDegistirici.sahneDegis(3);
+    }
+
+    public void tr_B()
+    {
+        SahneDegistirici.sahneDegis(4);
+    }
+
+    public void resimli_B()
+    {
+
+    }
+
+    public void cumle_B()
+    {
+
+    }
+}
