d4a373768477546ec15cad402ae7f527d196e7c9
osmanucar <63417248+osmanucar@users.noreply.github.com>
2021-01-10 15:24:32 +0300

    güncelleme

---
+++ Learnenglish/Assets/Script/SahneDegistirici.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SahneDegistirici : MonoBehaviour
+{
+    public static void sahneDegis(int sahne_id)
+    {
+        SceneManager.LoadScene(sahne_id);
+    }
+}
