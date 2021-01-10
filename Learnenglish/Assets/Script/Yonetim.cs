2c14cd14d3d5002267bef8444c477b598b6517af
osmanucar <63417248+osmanucar@users.noreply.github.com>
2021-01-10 15:25:33 +0300

    güncelleme

--- Learnenglish/Assets/Script/Yonetim.cs
+++ Learnenglish/Assets/Script/Yonetim.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Yonetim : MonoBehaviour
 {
@@ -22,11 +23,32 @@ public class Yonetim : MonoBehaviour
         }
     }
 
-   public void girisButton()
+   /* public void devamButonu()
+    {
+        
+        giris_P.SetActive(true);
+       // anamenu_P.SetActive(false);
+        SahneDegistirici.sahneDegis(0);
+        // SceneManager.LoadScene("Anamenu"); //Tekrar giriş alanına dönmek için.
+    }
+    */
+
+    public void girisButton()
     {
         PlayerPrefs.SetInt("girisDurumu", 1); //Daha önce giriş yapılmışşsa anamenu panelinden başla.
         giris_P.SetActive(false);
         anamenu_P.SetActive(true);
 
+    }
+
+    public void basla_B()
+    {
+        SahneDegistirici.sahneDegis(2);
+    }
+
+    public void cikis_B()
+    {
+        anamenu_P.SetActive(false);
+        giris_P.SetActive(true);
     }
 }
